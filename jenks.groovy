c6632b84b89bded2f9a1e6b17de974505cba0a70
niristotle okram <nirish.okram@gmail.com>
2016-10-19 14:03:14 -0500

    Adds place holder for deploy task

--- jenks.groovy
+++ jenks.groovy
@@ -5,13 +5,23 @@ node('Linux'){
         env.PATH = "${mvnHome}/bin:${env.PATH}"
 
     stage name: 'Git Clone', concurrency: 1
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'aa1c8452-6c57-40d4-814e-99ae1b74d1a9', url: 'git@github.com:okram999/mavenquick.git']]])
+        checkout([$class: 'GitSCM',
+        branches: [[name: '*/master']],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [], submoduleCfg: [],
+        userRemoteConfigs: [[credentialsId: 'aa1c8452-6c57-40d4-814e-99ae1b74d1a9', url: 'git@github.com:okram999/mavenquick.git']]])
 
     stage name: 'Build & Test', concurrency: 1
         sh 'mvn verify cobertura:cobertura sonar:sonar'
-        
-    stage name: 'Publish TestReport', concurrency: 1    
+
+    stage name: 'Publish TestReport', concurrency: 1
         junit '**\\target\\surefire-reports\\*.xml'
+
+    stage name: 'Deploy To Lab', concurrency: 1
+        //Add code to Deploy to Lab env
+
+
+
     }
 
     catch(err){
@@ -22,3 +32,4 @@ node('Linux'){
     }
 
 }
+
